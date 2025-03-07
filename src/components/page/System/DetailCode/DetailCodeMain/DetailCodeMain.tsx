e862acebb6afafebdf3081e870e39cca6edfdd9c
jsmin6330 <jsmin6330@gmail.com>
2025-03-07 12:01:56 +0900

    DetailCode 목록 조회 구현

--- src/components/page/System/DetailCode/DetailCodeMain/DetailCodeMain.tsx
+++ src/components/page/System/DetailCode/DetailCodeMain/DetailCodeMain.tsx
@@ -1,27 +1,73 @@
+import { useEffect, useState } from "react";
 import { StyledButton } from "../../../../common/StyledButton/StyledButton";
 import { DetailCodeMainStyled } from "./styled";
+import axios, { AxiosResponse } from "axios";
+import { Column, StyledTable } from "../../../../common/StyledTable/StyledTable";
+import { useLocation } from "react-router-dom";
+import { PageNavigate } from "../../../../common/pageNavigation/PageNavigate";
+
+
+interface IDetailCode {
+    groupCode: string,
+    groupName: string,
+    note: string,
+}
+
+interface IDetailCodeListResponse {
+    detailList: IDetailCode[],
+    detailCnt: number
+}
 
 export const DetailCodeMain = () => {
+    const { state } = useLocation();
+    const [detailList, setDetailList] = useState<IDetailCode[]>([]);
+    const [detailCnt, setDetailCnt] = useState<number>(0);
+    const [cPage, setCPage] = useState<number>(0);
+
+    const columns = [
+        { key: "groupCode", title: "공통코드" },
+        { key: "detailCode", title: "상세코드" },
+        { key: "detailName", title: "상세코드명" },
+        { key: "note", title: "비고" },
+        { key: "actions", title: "보기" },
+    ] as Column<IDetailCode>[];
+
+    useEffect(() => {
+        searchDetailCodeList();
+    }, [])
+
+    const searchDetailCodeList = (currentPage?: number) => {
+        currentPage = currentPage || 1;
+
+        axios.post("/system/detailListBody", {
+            groupCode: state.groupCode,
+            pageSize: 5,
+            currentPage,
+        }).then((res: AxiosResponse<IDetailCodeListResponse>) => {
+            setDetailList(res.data.detailList);
+            setDetailCnt(res.data.detailCnt);
+            setCPage(currentPage);
+        })
+    }
+
     return (
         <DetailCodeMainStyled>
-            <table>
-                <thead>
-                    <tr>
-                        <th>번호</th>
-                        <th>그룹코드</th>
-                        <th>상세코드</th>
-                        <th>상세코드명</th>
-                        <th>상세코드설명</th>
-                        <th>사용여부</th>
-                        <th>비고</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td colSpan={7}>조회 내역이 없습니다.</td>
-                    </tr>
-                </tbody>
-            </table>
+            <StyledTable
+                columns={columns}
+                data={detailList}
+                hoverable={true}
+                fullWidth={true}
+                renderAction={(row) =>
+                    <StyledButton size="small" onClick={(e) => { }}>
+                        수정
+                    </StyledButton>}
+            />
+            <PageNavigate
+                totalItemsCount={detailCnt}
+                activePage={cPage}
+                itemsCountPerPage={5}
+                onChange={searchDetailCodeList}
+            />
             <StyledButton>뒤로가기</StyledButton>
         </DetailCodeMainStyled>
     );
