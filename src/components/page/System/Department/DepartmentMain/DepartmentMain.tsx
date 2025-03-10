c8eb4578be8ed82e1d26f2774b187de1c324623a
jsmin6330 <jsmin6330@gmail.com>
2025-03-10 10:02:17 +0900

    Department 리스트 조회 구현

--- src/components/page/System/Department/DepartmentMain/DepartmentMain.tsx
+++ src/components/page/System/Department/DepartmentMain/DepartmentMain.tsx
@@ -1,6 +1,62 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react"
+import { Column, StyledTable } from "../../../../common/StyledTable/StyledTable";
+import { PageNavigate } from "../../../../common/pageNavigation/PageNavigate";
+import { DepartmentMainStyled } from "./styled";
+
+
+interface IDepartment {
+    detailCode: string,
+    detailName: string
+}
+
+interface IDepartmentListBodyResponse {
+    departmentList: IDepartment[],
+    departmentCnt: number
+}
+
 export const DepartmentMain = () => {
+    const [departmentList, setDepartmentList] = useState<IDepartment[]>([]);
+    const [departmentCnt, setDepartmentCnt] = useState<number>(0);
+    const [cPage, setCPage] = useState<number>(0);
+
+    const columns = [
+        { key: "detailCode", title: "부서코드" },
+        { key: "detailName", title: "부서명" },
+    ] as Column<IDepartment>[];
+
+    useEffect(() => {
+        searchDepartmentList();
+    }, [])
+
+    const searchDepartmentList = (currentPage?: number) => {
+        currentPage = currentPage || 1;
+        const searchParam = new URLSearchParams();
+        searchParam.append("currentPage", currentPage.toString());
+        searchParam.append("pageSize", "5");
+
+        axios.post("/system/departmentListBody", searchParam)
+            .then((res: AxiosResponse<IDepartmentListBodyResponse>) => {
+                setDepartmentList(res.data.departmentList);
+                setDepartmentCnt(res.data.departmentCnt);
+                setCPage(currentPage);
+            })
+    }
+
     return (
-        <>
-        </>
+        <DepartmentMainStyled>
+            <StyledTable
+                columns={columns}
+                data={departmentList}
+                hoverable={true}
+                fullWidth={true}
+            />
+            <PageNavigate
+                totalItemsCount={departmentCnt}
+                activePage={cPage}
+                itemsCountPerPage={5}
+                onChange={searchDepartmentList}
+            />
+        </DepartmentMainStyled>
     )
 }
---
+++ src/components/page/System/Department/DepartmentMain/styled.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const DepartmentMainStyled = styled.div`
+    margin-top: 5px;
+`;
