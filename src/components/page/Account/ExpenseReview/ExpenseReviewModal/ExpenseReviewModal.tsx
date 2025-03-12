553174ebdf10a9548d788da57a3c2c7b46d16da8
havanaOh <top2480@naver.com>
2025-03-12 15:03:17 +0900

    병합 오류 수정

--- src/components/page/Account/ExpenseReview/ExpenseReviewModal/ExpenseReviewModal.tsx
+++ src/components/page/Account/ExpenseReview/ExpenseReviewModal/ExpenseReviewModal.tsx
@@ -105,7 +105,6 @@ export const ExpenseReviewModal: FC<IExpenseReviewModalProps> = ({ expenseDetail
             <div className='container'>
                 <form ref={formRef}>
                     <ModalStyledTable>
-                    <ModalStyledTable>
                         <tbody>
                             <tr>
                                 <th>결의번호</th>
