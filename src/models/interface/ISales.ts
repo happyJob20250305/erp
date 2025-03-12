eebb7eabe4a467ab190dfc944b6b7abe92f5098c
choiwanmin <choiwanmin@google.com>
2025-03-12 17:27:33 +0900

    ISales Sales 영역에서 사용되는 인터페이스 파일 생성

---
+++ src/models/interface/ISales.ts
@@ -0,0 +1,66 @@
+export interface ISalesPlan {
+    client_id: number;
+    client_name: string;
+    detail_code: string;
+    detail_name: string;
+    emp_id: number;
+    emp_name: string;
+    goal_quanti: number;
+    group_name: string;
+    industry_code: string;
+    manufacturer_id: number;
+    name: string;
+    perform_qut: number;
+    plan_memo: string;
+    plan_num: number;
+    product_name: string;
+    target_date: string;
+}
+
+export interface ISalesPlanResponse {
+    searchPlanList: ISalesPlan[];
+    salesCnt: number;
+}
+
+export interface IManufacturer {
+    industryCode?: string;
+    industryName: string;
+    manufacturer_id: number;
+}
+
+export interface IManufacturerResponse {
+    manuFacturerList: IManufacturer[];
+    unitIndustrycode: string;
+    manufacturerCnt: number;
+}
+
+export interface IMaincategory {
+    group_code: string;
+    group_name: string;
+}
+
+export interface IMaincategoryResponse {
+    mainCategory: IMaincategory[];
+    mainCategoryCnt: number;
+}
+
+export interface ISubcategory {
+    detail_code: string;
+    detail_name: string;
+}
+
+export interface ISubcategoryResponse {
+    subCategory: ISubcategory[];
+    subcategoryCnt: number;
+}
+
+export interface IProduct {
+    industry_code: string;
+    name: string;
+    product_code: string;
+}
+
+export interface IProductResponse {
+    productList: IProduct[];
+    productCnt: number;
+}
