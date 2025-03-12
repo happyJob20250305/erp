4aab1634e408ec35be749d18ebc49564b4c38436
havanaOh <top2480@naver.com>
2025-03-12 15:37:40 +0900

    manage 충돌 후 수정

--- src/components/page/Account/ExpenseReview/ExpenseReviewModal/ExpenseReviewModal.tsx
+++ src/components/page/Account/ExpenseReview/ExpenseReviewModal/ExpenseReviewModal.tsx
@@ -5,13 +5,9 @@ import { ExpenseReviewModalStyle } from "./styled";
 import { modalState } from "../../../../../stores/modalState";
 import { FC, useEffect, useRef, useState } from "react";
 import { ISetListOption } from "../../Manage/ManageSearch.tsx/ManageSearch";
-import { ILoginInfo } from "../../../../../models/interface/store/userInfo";
-import { loginInfoState } from "../../../../../stores/userInfo";
-import moment from "moment";
 import axios, { AxiosResponse } from "axios";
 import { IExpenseReview } from "../ExpenseReviewMain/ExpenseReviewMain";
 import { StyledSelectBox } from "../../../../common/StyledSelectBox/StyledSelectBox";
-import { Ratio } from "react-bootstrap";
 import { ButtonArea, ModalStyledTable } from "../../VoucherList/VoucherListModal/styled";
 
 interface IExpenseReviewModalProps {
