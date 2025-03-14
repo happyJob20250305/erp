a21c02562a68576b4b2192d50e4a4c9757386908
choiwanmin <choiwanmin@google.com>
2025-03-14 12:03:14 +0900

    OrderList OrderInfoProvide추가 -> return에 추가

--- src/pages/Business/OrderList.tsx
+++ src/pages/Business/OrderList.tsx
@@ -1,13 +1,18 @@
+import { OrderInfoContext, OrderInfoProvider } from "../../api/Provider/OrderInfoProvider";
 import { ContentBox } from "../../components/common/ContentBox/ContentBox";
 import { OrderListMain } from "../../components/page/Business/Order/OrderList/OrderListMain/OrderListMain";
+import { OrderListSearch } from "../../components/page/Business/Order/OrderList/OrderListSearch/OrderListSearch";
 
 export const OrderList = () => {
     return (
         <>
-            <ContentBox variant='primary' fontSize='large'>
-                수주 조회/작성
-            </ContentBox>
-            <OrderListMain />
+            <OrderInfoProvider>
+                <ContentBox variant='primary' fontSize='large'>
+                    수주 조회/작성
+                </ContentBox>
+                <OrderListSearch />
+                <OrderListMain />
+            </OrderInfoProvider>
         </>
     );
 };
