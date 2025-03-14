2414719f26da83eaed63406537160d1d89632a62
minjjings <banggujoong@gmail.com>
2025-03-14 13:58:45 +0900

    [Feat] 승진 내역 관리 페이지

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,6 +10,12 @@ export const Employee = {
     employeeDetail: "/personnel/employeeDetail.do",
     emplStatusUpdate: "/personnel/emplStatusUpdate.do",
 };
+
+export const Promotion = {
+    promitionList: "/personnel/promotionList.do",
+    promitionDetailList: "/personnel/promotionDetail.do",
+};
+
 export const SalaryManager = {
     salaryList: "/personnel/salaryList.do",
     empnoSalaryList: "/personnel/salaryDetail.do",
---
+++ src/components/page/Personnel/promotion/PromotionDetail/PromotionDetail.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import {
+    IPromotionDetail,
+    IPromotionDetailResponse,
+    PromotionMainProps,
+} from "../../../../../models/interface/personnel/promotion/IPromotionDetail";
+import { searchApi } from "../../../../../api/SystemApi/searchApi";
+import { Promotion } from "../../../../../api/api";
+import { Column, StyledTable } from "../../../../common/StyledTable/StyledTable";
+import { PromotionDetailStyled } from "./styled";
+
+export const PromotionDetail = ({ data }: PromotionMainProps) => {
+    const [promotionDetailList, setPromotionDetailList] = useState<IPromotionDetail[]>();
+    const columns = [
+        { key: "createdAt", title: "발령일자" },
+        { key: "jobGrade", title: "발령내용" },
+    ] as unknown as Column<IPromotionDetail>[];
+    useEffect(() => {
+        PromitionDetailList(data);
+    }, [data]);
+
+    const PromitionDetailList = async (data: number) => {
+        const searchParam = new URLSearchParams();
+        searchParam.append("currentPage", "1");
+        searchParam.append("pageSize", "5");
+        searchParam.append("employeeNumber", String(data));
+
+        const result = await searchApi<IPromotionDetailResponse>(Promotion.promitionDetailList, searchParam);
+        setPromotionDetailList(result.promotionDetailList);
+    };
+
+    return (
+        <>
+            <PromotionDetailStyled>
+                <StyledTable data={promotionDetailList} columns={columns} fullWidth={true} />
+            </PromotionDetailStyled>
+        </>
+    );
+};
